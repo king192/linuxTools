82ff2e014ee13085063c5d2e7b0993162ad8e701
xhq <1434970057@qq.com>
2017-04-07 09:28:29 +0800

    170407.2

--- web_log_analysis/log.sh
+++ web_log_analysis/log.sh
@@ -1,37 +1,38 @@
+#!/bin/bash
 date=$(date +%d/%b/%Y)
 echo "百度蜘蛛访问次数："
 cd /home/wwwlogs && cat yunshare.net.log | grep ${date} | grep 'Baiduspider' | wc -l
 echo "百度蜘蛛访问404次数："
-cat yunshare.net.log | grep ${date} | grep 'Baiduspider' | grep '404' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'Baiduspider' | awk -F ' ' '{print $9}' | grep '404' | wc -l
 echo "百度蜘蛛访问200次数："
-cat yunshare.net.log | grep ${date} | grep 'Baiduspider' | grep '200' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'Baiduspider' | awk -F ' ' '{print $9}' | grep '200' | wc -l
 echo "百度蜘蛛访问403次数："
-cat yunshare.net.log | grep ${date} | grep 'Baiduspider' | grep '403' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'Baiduspider' | awk -F ' ' '{print $9}' | grep '403' | wc -l
 
 echo "必应蜘蛛访问次数："
 cat yunshare.net.log | grep ${date} | grep 'bingbot' | wc -l
 echo "必应蜘蛛访问404次数："
-cat yunshare.net.log | grep ${date} | grep 'bingbot' | grep '404' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'bingbot' | awk -F ' ' '{print $9}' | grep '404' | wc -l
 echo "必应蜘蛛访问200次数："
-cat yunshare.net.log | grep ${date} | grep 'bingbot' | grep '200' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'bingbot' | awk -F ' ' '{print $9}' | grep '200' | wc -l
 echo "必应蜘蛛访问403次数："
-cat yunshare.net.log | grep ${date} | grep 'bingbot' | grep '403' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'bingbot' | awk -F ' ' '{print $9}' | grep '403' | wc -l
 
 echo "谷歌蜘蛛访问次数："
 cat yunshare.net.log | grep ${date} | grep 'Googlebot' | wc -l
 echo "谷歌蜘蛛访问404次数："
-cat yunshare.net.log | grep ${date} | grep 'Googlebot' | grep '404' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'Googlebot' | awk -F ' ' '{print $9}' | grep '404' | wc -l
 echo "谷歌蜘蛛访问200次数："
-cat yunshare.net.log | grep ${date} | grep 'Googlebot' | grep '200' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'Googlebot' | awk -F ' ' '{print $9}' | grep '200' | wc -l
 echo "谷歌蜘蛛访问403次数："
-cat yunshare.net.log | grep ${date} | grep 'Googlebot' | grep '403' | wc -l
+cat yunshare.net.log | grep ${date} | grep 'Googlebot' | awk -F ' ' '{print $9}' | grep '403' | wc -l
 
 echo "360蜘蛛访问次数："
 cat yunshare.net.log | grep ${date} | grep '360Spider' | wc -l
 echo "360蜘蛛访问404次数："
-cat yunshare.net.log | grep ${date} | grep '360Spider' | grep '404' | wc -l
+cat yunshare.net.log | grep ${date} | grep '360Spider' | awk -F ' ' '{print $9}' | grep '404' | wc -l
 echo "360蜘蛛访问200次数："
-cat yunshare.net.log | grep ${date} | grep '360Spider' | grep '200' | wc -l
+cat yunshare.net.log | grep ${date} | grep '360Spider' | awk -F ' ' '{print $9}' | grep '200' | wc -l
 echo "360蜘蛛访问403次数："
-cat yunshare.net.log | grep ${date} | grep '360Spider' | grep '403' | wc -l
+cat yunshare.net.log | grep ${date} | grep '360Spider' | awk -F ' ' '{print $9}' | grep '403' | wc -l
 
